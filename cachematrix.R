d26d3d45bb0c90c2119bb88a475ee46f146f4e6b
Sivarat Malapet <smalapet@gmail.com>
2014-07-22 19:29:23 +0700

    completed function

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,38 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## Example of input square matrix c=rbind(c(1, -1/4), c(-1/4, 1))
 
+## Function makeCacheMatrix created for store matrix object value in an environment   
+makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  
+  set <- function(y){
+    x <<- y
+    m <<- NULL
+  }
+  
+  get <- function() x
+  
+  setInverse <- function(mtrx) m <<- mtrx
+  
+  getInverse <- function() m
+  
+  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## Function cacheSolve created for calculate inverse matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  m <- x$getInverse()
+  if(!is.null(m)){
+    message("getting inversed of a matrix")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  #x$setInverse(m)
+  m
 }
